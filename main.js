e0788bcd22ff8d1c7c091b6945090282ccb7281c
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-14 14:47:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
-ctx.arc (220, 200, 40, 0 ,2*MathPI);
+ctx.arc (220, 200, 40, 0 ,2*Math.PI);
 ctx.stroke(); 
 
 ctx.beginPath();
 ctx.strokeStyle = "black";
-ctxx.lineWidth = 5;
-ctx.arc (300, 210, 40, 0 ,2*MathPI);
+ctx.lineWidth = 5;
+ctx.arc (300, 210, 40, 0 ,2*Math.PI);
 ctx.stroke(); 
 
 ctx.beginPath();
 ctx.strokeStyle = "red";
 ctx.lineWidth = 5;
-ctx.arc (380, 210, 40, 0 ,2*MathPI);
+ctx.arc (380, 210, 40, 0 ,2*Math.PI);
 ctx.stroke(); 
 
 ctx.beginPath();
 ctx.strokeStyle = "yellow";
 ctx.lineWidth = 5;
-ctx.arc (220, 200, 40, 0 ,2*MathPI);
+ctx.arc (220, 200, 40, 0 ,2*Math.PI);
 ctx.stroke(); 
 
 ctx.beginPath();
 ctx.strokeStyle = "green";
 ctx.lineWidth = 5;
-ctx.arc (300, 200, 40, 0 ,2*MathPI);
+ctx.arc (300, 200, 40, 0 ,2*Math.PI);
 ctx.stroke(); 
 
 
