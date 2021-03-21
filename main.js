b22e38f91b6a873acc9781620b29cc1077d6553e
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-21 13:05:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ canvas = document.getElementById("canvasa");
 ctx = canvas.getContext("2d");
 
 ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 5;
+ctx.rect (150, 140, 430, 200);
+ctx.stroke(); 
+
+ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
 ctx.arc (250, 210, 40, 0 ,2*Math.PI);
